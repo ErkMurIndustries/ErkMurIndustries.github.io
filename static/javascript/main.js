336184177875c32cb81bcbb4c111350792cc88d3
Erik <erikjmurray@gmail.com>
2023-05-25 10:17:00 -0400

    menu testing, blog template

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -16,7 +16,22 @@ menuLink.addEventListener("mouseout", function() {
 menuLink.addEventListener("click", function() {
     if (menuItems.style.display === "none") {
       menuItems.style.display = "";
+      animateMenuItems()
     } else {
       menuItems.style.display = "none";
     }
 });
+
+
+function animateMenuItems() {
+  var items = menuItems.getElementsByClassName("menu-item");
+  var delay = 100; // Delay in milliseconds between each menu item
+
+  for (var i = 0; i < items.length; i++) {
+    (function(index) {
+      setTimeout(function() {
+        items[index].style.opacity = 1;
+      }, delay * index);
+    })(i);
+  }
+}
